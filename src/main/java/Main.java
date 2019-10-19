1b0ee0bd34172dbe6aced3e92655b946f6a8cd7b
Jon <1246634075@qq.com>
2019-10-20 00:00:55 +0800

    作业来了

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,11 +7,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Data data = new Data();
-        ArrayList<Student> students = data.datas.get(0);
-        Student student = students.get(0);
-        Turn turn = student.getSuitableTurns().get(0);
-        turn.getDayTime();
-        turn.getWorkDay();
         //TODO 在这里开始你的程序
     }
 }
