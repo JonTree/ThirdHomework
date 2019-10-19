b3d1990fb23baba853d9255bd9a3d1aa82b9b9e4
Jon <1246634075@qq.com>
2019-10-20 02:06:39 +0800

    更新Gradle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Data data = new Data();
+        System.out.println(data.datas);;
+        System.out.println();
         //TODO 在这里开始你的程序
     }
 }
