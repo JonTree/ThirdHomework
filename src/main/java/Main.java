00def6b964c3404f2ee8368a1dcb31b5b8fda2c9
Jon <1246634075@qq.com>
2019-10-26 21:04:43 +0800

    updata

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,7 @@
-import scheduling.Schedule;
 import data.Data;
 
 public class Main {
     public static void main(String[] args) {
         Data data = new Data();
-        Schedule s = new Schedule();
-        for (int i = 0; i < data.datas.size(); i++) {
-            System.out.println("根据data" + (i + 1) + "得出下表:");
-            s.getTable(data.datas.get(i));
-            s.print();
-        }
     }
 }
